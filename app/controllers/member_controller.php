73ef5dc0dfda9dc07612cae0eed09dc9b3bdf040
quentic <christian.quentin@transparence-c.com>
2016-04-21 15:26:30 +0200

    affichage de la page index des stagiaires

---
+++ app/controllers/member_controller.php
@@ -0,0 +1,58 @@
+<?php
+
+// put full path to Smarty.class.php
+require('../../smarty/libs/Smarty.class.php');
+$smarty = new Smarty();
+
+$smarty->setCompileDir('../../smarty/templates_c');
+$smarty->setCacheDir('../../smarty/cache');
+$smarty->setConfigDir('../../smarty/configs');
+
+$smarty->setTemplateDir('../../app/views/members');
+?>
+
+<?php
+
+    class member{
+        function member($id, $nom, $prenom){
+            $this->id = $id;
+            $this->nom = $nom;
+            $this->prenom = $prenom;
+        }
+    }
+
+    function index($smarty){
+        // Remplacer ces 2 lignes par un appel mysql sur tous les stages
+        $member1 = new member('1', 'Jeandel', 'Thierry');
+        $member2 = new member('2', 'Quentin', 'Christian');
+        $member3 = new member('3', 'Grandmougin', 'Xavier');
+
+        $smarty->assign('members', array($member1, $member2, $member3));
+        $smarty->display('index.html');
+    }
+
+    switch ($_GET['action']) {
+        case 'new':
+            break;
+
+        case 'create':
+            break;
+
+        case 'edit':
+            break;
+
+        case 'update':
+            break;
+
+        case 'show':
+            break;
+
+        case 'delete':
+            break;
+
+        default:
+            # Affiche la page index par défaut
+            index($smarty);
+    }
+
+?>
