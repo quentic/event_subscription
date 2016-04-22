7190fde4af3f22250184ed28a4761558fbb4ca8f
quentic <christian.quentin@transparence-c.com>
2016-04-22 17:27:28 +0200

    renommage 'stage' en 'event'
    pour internationalisation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,16 +1,16 @@
 
 $(document).ready(function() {
 	$("input.inscrire").click(function(){
-		stage_id = $(this).attr("data-stage-id");
+		event_id = $(this).attr("data-event-id");
 		member_id = $(this).attr("data-member-id");
 
 		inscription = $(this).is(':checked');
 		if (inscription) {
 			alert("inscription enregistrée");
-			window.location.href = "events_member_controller.php?action=create&stage_id=" + stage_id + "&member_id=" + member_id ;
+			window.location.href = "events_member_controller.php?action=create&event_id=" + event_id + "&member_id=" + member_id ;
 		} else {
 			alert("désinscription enregistrée");
-			window.location.href = "events_member_controller.php?action=delete&stage_id=" + stage_id + "&member_id=" + member_id ;
+			window.location.href = "events_member_controller.php?action=delete&event_id=" +event_id + "&member_id=" + member_id ;
 		}
 	})
 })
