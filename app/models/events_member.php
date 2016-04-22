e955740079cd61e78d320bfdcae957411a8541bb
quentic <christian.quentin@transparence-c.com>
2016-04-22 17:27:53 +0200

    inscription/désinscription => en mysql !

--- app/models/events_member.php
+++ app/models/events_member.php
@@ -17,11 +17,11 @@
     }
 
     function associer($event_id, $member_id){
-      // mysql_query("INSERT INTO events_members (event_id, member_id) VALUE (" . $event_id . "," . $member_id . ")");
+      $result = mysql_query("INSERT INTO events_members (event_id, member_id) VALUE ($event_id, $member_id)");
       }
 
     function dissocier($event_id, $member_id){
-      // mysql_query("DELETE FROM events_members WHERE event_id=" . $event_id . " AND member_id=" . $member_id);
+      $result = mysql_query("DELETE FROM events_members WHERE event_id=$event_id AND member_id= $member_id");
       }
   }
 
