1ec5e15fedb1135f486ba911619dc81802618426
quentic <christian.quentin@transparence-c.com>
2016-04-22 10:53:01 +0200

    configuration mysql

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+
+  // Connexion et sélection de la base
+  $hostname = 'localhost';
+  $username = 'root';
+  $password = '';
+  $db_name = 'o2';
+
+  $conn = mysql_connect($hostname, $username, $password)
+    or die('Impossible de se connecter : ' . mysql_error());
+  //echo 'Connected successfully';
+
+  mysql_select_db($db_name) or die('Impossible de sélectionner la base de données');
+
+  mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $conn);
+?>
