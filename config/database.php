3f6df7154b8c791810779c998f765a4962a625f8
Christian <christian.quentin@transparence-c.com>
2016-04-23 10:49:06 +0200

    initialisation de la base o2

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
   // Connexion et sélection de la base
   $hostname = 'localhost';
   $username = 'root';
-  $password = '';
+  $password = 'root';
   $db_name = 'o2';
 
   $conn = mysql_connect($hostname, $username, $password)
@@ -15,25 +15,25 @@
   mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $conn);
   
   // o2 database table creation
-  funtion database_table_init(){
-	  mysql_query("CREATE TABLE IF NOT EXISTS members
-		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		nom VARCHAR(100),
-		prenom VARCHAR(100)
-		)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
-	  ");  
+  //funtion database_table_init(){
+	  //mysql_query("CREATE TABLE IF NOT EXISTS members
+		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		//nom VARCHAR(100),
+		//prenom VARCHAR(100)
+		//)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
+	  //");  
 
-	  mysql_query("CREATE TABLE IF NOT EXISTS events
-		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-		nom VARCHAR(100),
-		periode VARCHAR(100)
-		)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
-	  "); 
+	  //mysql_query("CREATE TABLE IF NOT EXISTS events
+		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		//nom VARCHAR(100),
+		//periode VARCHAR(100)
+		//)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
+	  //"); 
 
-	  mysql_query("CREATE TABLE IF NOT EXISTS events_members
-		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event_id int, 
-		member_id int, UNIQUE (event_id, member_id) )  
-		ENGINE='MyISAM';
-	  "); 
-  }
+	  //mysql_query("CREATE TABLE IF NOT EXISTS events_members
+		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event_id int, 
+		//member_id int, UNIQUE (event_id, member_id) )  
+		//ENGINE='MyISAM';
+	  //"); 
+  //}
 ?>
