5e7bdd15b06237f5eca9409d78eeddefe1bbe767
Christian <christian.quentin@transparence-c.com>
2016-04-23 10:34:05 +0200

    initialisation de la base o2

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
   // Connexion et sélection de la base
   $hostname = 'localhost';
   $username = 'root';
-  $password = 'Pass123.';
+  $password = '';
   $db_name = 'o2';
 
   $conn = mysql_connect($hostname, $username, $password)
@@ -13,4 +13,27 @@
   mysql_select_db($db_name) or die('Impossible de sélectionner la base de données');
 
   mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $conn);
+  
+  // o2 database table creation
+  funtion database_table_init(){
+	  mysql_query("CREATE TABLE IF NOT EXISTS members
+		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		nom VARCHAR(100),
+		prenom VARCHAR(100)
+		)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
+	  ");  
+
+	  mysql_query("CREATE TABLE IF NOT EXISTS events
+		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+		nom VARCHAR(100),
+		periode VARCHAR(100)
+		)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
+	  "); 
+
+	  mysql_query("CREATE TABLE IF NOT EXISTS events_members
+		(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event_id int, 
+		member_id int, UNIQUE (event_id, member_id) )  
+		ENGINE='MyISAM';
+	  "); 
+  }
 ?>
