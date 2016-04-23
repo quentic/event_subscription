3085eaa22b836689f6e5f8d822587437c57ed97e
Christian <christian.quentin@transparence-c.com>
2016-04-23 16:22:47 +0200

    Stage/Event : créer un nouveau

--- app/controllers/event_controller.php
+++ app/controllers/event_controller.php
@@ -11,34 +11,54 @@ $smarty->setTemplateDir('../../app/views/events');
 <?php
 
     function index($smarty){
-        $event = new Event();
-        $smarty->assign('events',$event->all());
+        $event = new Event($_POST);
 
+        $smarty->assign('events', $event->all());
         $smarty->display('index.html');
     }
 
-    switch ($_GET['action']) {
-        case 'new':
-            break;
-
-        case 'create':
-            break;
-
-        case 'edit':
-            break;
-
-        case 'update':
-            break;
-
-        case 'show':
-            break;
-
-        case 'delete':
-            break;
-
-        default:
-            # Affiche la page index par défaut
-            index($smarty);
+    function new_m($smarty){
+        $smarty->display('new.html');
     }
+
+    function save($smarty){
+        $event = new Event($_POST);
+        $event->save();
+
+        $smarty->assign('events', $event->all());
+        $smarty->display('index.html');
+    }
+
+    if (!isset($_GET["action"]))
+      # Affiche la page index par défaut
+      index($smarty);
+
+    else
+      switch ($_GET["action"]) {
+          case 'new':
+              # Affiche la page new.html
+              new_m($smarty);
+              break;
+
+          case 'create':
+              save($smarty);
+              break;
+
+          case 'edit':
+              break;
+
+          case 'update':
+              break;
+
+          case 'show':
+              break;
+
+          case 'delete':
+              break;
+
+          default:
+              # Affiche la page index par défaut
+              index($smarty);
+      }
 
 ?>
--- app/models/event.php
+++ app/models/event.php
@@ -1,20 +1,30 @@
 <?php
+// un stage auquel peuvent s'inscrire des stagiaires
+// an event which members can subscribe to
+class Event{
 
-  // un stage auquel peuvent s'inscrire des stagiaires
-  // an event which members can subscribe to
-  class Event{
-
-    // Sélectionner tous les stages
-    function all() {
-      $query = 'SELECT * FROM events';
-      $result = mysql_query($query) or die('Échec de la requête : ' . mysql_error());
-      $t_result = [];
-
-      while ($line = mysql_fetch_object($result)) {
-        $t_result[] = $line;
-      }
-      return $t_result;
-    }
+  function __construct($t_post){
+    $this->nom = $t_post['nom' ];
+    $this->periode = $t_post['periode'];
   }
+    
+   // Sélectionner tous les stages
+   function all() {
+     $query = 'SELECT * FROM events';
+     $result = mysql_query($query) or die('Échec de la requête : ' . mysql_error());
+     $t_result = [];
+
+     while ($line = mysql_fetch_object($result)) {
+       $t_result[] = $line;
+     }
+     return $t_result;
+   }
+    
+   // enregistrer un nouvel enregistrement dans la base
+   function save(){
+     $query = "INSERT INTO events (nom, periode) VALUES ('$this->nom', '$this->periode')";
+     $result = mysql_query($query) or die('Échec de la requête : ' . mysql_error() . $query);
+     }
+}
 
 ?>
