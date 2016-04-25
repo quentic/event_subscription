0784e61cbe494a29264abb9e83e886c21c484504
quentic <christian.quentin@transparence-c.com>
2016-04-25 10:21:16 +0200

    on peut maintenant masquer les vieux events/stages

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@
   mysql_select_db($db_name) or die('Impossible de sélectionner la base de données');
 
   mysql_query("SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'", $conn);
-  
+
   // o2 database table creation
   //funtion database_table_init(){
 	  //mysql_query("CREATE TABLE IF NOT EXISTS members
@@ -21,19 +21,20 @@
 		//nom VARCHAR(100),
 		//prenom VARCHAR(100)
 		//)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
-	  //");  
+	  //");
 
 	  //mysql_query("CREATE TABLE IF NOT EXISTS events
 		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 		//nom VARCHAR(100),
-		//periode VARCHAR(100)
+		//periode VARCHAR(100),
+		//masquer TINYINT(1) NOT NULL DEFAULT '0'
 		//)  ENGINE='MyISAM' COLLATE 'utf8_general_ci';
-	  //"); 
+	  //");
 
 	  //mysql_query("CREATE TABLE IF NOT EXISTS events_members
-		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event_id int, 
-		//member_id int, UNIQUE (event_id, member_id) )  
+		//(id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, event_id int,
+		//member_id int, UNIQUE (event_id, member_id) )
 		//ENGINE='MyISAM';
-	  //"); 
+	  //");
   //}
 ?>
