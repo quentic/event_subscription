be556ce3c5f383ce75b7ecac562c2f2178e2617e
quentic <christian.quentin@transparence-c.com>
2016-04-28 16:52:14 +0200

    isolement du javascript spécifique inscriptions

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,38 +1,10 @@
 
 $(document).ready(function() {
-	$("td.inscription input").click(function(){
-		event_id = $(this).attr("data-event-id");
-		member_id = $(this).attr("data-member-id");
 
-		inscription = $(this).is(':checked');
-		if (inscription) {
-      // Inscription
-      $.post("events_member_controller.php?action=create",
-          {
-            event_id: event_id,
-            member_id: member_id
-          },
-          function(data, status){
-            alert('Inscription ' + (status=='success' ? '' : 'non ') + 'enregistrée');
-          });
-          
-		} else {
-      // Désinscription
-      $.post("events_member_controller.php?action=destroy",
-          {
-            event_id: event_id,
-            member_id: member_id
-          },
-          function(data, status){
-            alert('Désinscription ' + (status=='success' ? '' : 'non ') + 'enregistrée');
-          });	
-		}
-	});
-  
   // Affiche les calendriers
   $.datepicker.setDefaults({
       showMonthAfterYear: true,
-      changeYear: true,
+      changeYear: true
   });
   $.datepicker.setDefaults( $.datepicker.regional['fr'] );
   $("input[type=date]").datepicker({ dateFormat: "yy-mm-dd" });
---
+++ app/assets/javascripts/events_members.js
@@ -0,0 +1,32 @@
+
+$(document).ready(function() {
+	$("td.inscription input").click(function(){
+		event_id = $(this).attr("data-event-id");
+		member_id = $(this).attr("data-member-id");
+
+		inscription = $(this).is(':checked');
+		if (inscription) {
+      // Inscription
+      $.post("events_member_controller.php?action=create",
+          {
+            event_id: event_id,
+            member_id: member_id
+          },
+          function(data, status){
+            alert('Inscription ' + (status=='success' ? '' : 'non ') + 'enregistrée');
+          });
+
+		} else {
+      // Désinscription
+      $.post("events_member_controller.php?action=destroy",
+          {
+            event_id: event_id,
+            member_id: member_id
+          },
+          function(data, status){
+            alert('Désinscription ' + (status=='success' ? '' : 'non ') + 'enregistrée');
+          });
+		}
+	});
+
+})
