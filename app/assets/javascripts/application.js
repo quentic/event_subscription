aeaa3b9355fec535c5050c0f751b21b8bf5d7e7b
quentic <christian.quentin@transparence-c.com>
2016-04-28 12:01:56 +0200

    datepicker sur les champs "date"

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -27,5 +27,14 @@ $(document).ready(function() {
             alert('Désinscription ' + (status=='success' ? '' : 'non ') + 'enregistrée');
           });	
 		}
-	})
+	});
+  
+  // Affiche les calendriers
+  $.datepicker.setDefaults({
+      showMonthAfterYear: true,
+      changeYear: true,
+  });
+  $.datepicker.setDefaults( $.datepicker.regional['fr'] );
+  $("input[type=date]").datepicker({ dateFormat: "yy-mm-dd" });
+
 })
