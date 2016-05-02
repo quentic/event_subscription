4860dfda648a8852d6b54461afbe4647517ac1b2
quentic <christian.quentin@transparence-c.com>
2016-05-02 09:42:06 +0200

    DRY : factorisation de l'aiguillage des actions

---
+++ app/controllers/application.php
@@ -0,0 +1,36 @@
+<?php
+    # Module d'aiguillage des actions commun à tous les controleurs
+
+    # récupère les paramètres action et id dans l'URL, s'ils existent
+    $action = isset($_GET['action']) ? $_GET['action'] : '';
+    $id = isset($_GET['id']) ? $_GET['id'] : '';
+
+    # analyse l'action demandée
+    switch ($action) {
+      case 'new':
+        # Affiche la page new.html
+        new_m($smarty);
+        break;
+
+      case 'create':
+        create($smarty);
+        break;
+
+      case 'edit':
+        edit($smarty, $id);
+        break;
+
+      case 'update':
+        update($smarty, $id);
+        break;
+
+      case 'destroy':
+        destroy($smarty, $id);
+        break;
+
+      default:
+        # Affiche la page index par défaut
+        index($smarty);
+    }
+
+?>
