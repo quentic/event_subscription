7e4e289e0d66a5d0b2ceb11eed19928614d7f98d
quentic <christian.quentin@transparence-c.com>
2016-05-02 09:36:35 +0200

    obsolète depuis la requête complexe pour les inscriptions

--- app/models/member.php
+++ app/models/member.php
@@ -63,18 +63,6 @@ class Member{
     return $t_result;
   }
 
-  # Sélectionner les members/stagiaires actifs
-  function actifs() {
-    $query = 'SELECT * FROM active_members';
-    $result = mysql_query($query) or die('Échec de la requête : ' . mysql_error());
-    $t_result = [];
-
-    while ($line = mysql_fetch_object($result)) {
-      $t_result[] = $line;
-    }
-    return $t_result;
-  }
-
   # enregistrer un nouveau membre/stagiaire dans la base
   function save(){
     $query = "INSERT INTO members (nom, prenom)
